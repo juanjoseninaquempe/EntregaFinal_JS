bb9c0e1b81910dc090d1f436dcb304bdd4d9f37e
JUAN jose <jnina@frba.utn.edu.ar>
2023-06-15 14:28:56 -0300

    Nueva actualizacion

--- src/index.js
+++ src/index.js
@@ -1,27 +1,74 @@
-import $ from  'jquery'
-import {getPokemon} from './api'
+// import $ from  'jquery'
+// import {getPokemon} from './api'
 
-obtenerPokemon()
+// obtenerPokemon()
 
-function obtenerPokemon(){
-    let random = numeroAleatorio(150)+1
+// function obtenerPokemon(){
+//     let random = numeroAleatorio(150)+1
 
-    let pokemon = getPokemon(1)
-    pokemon.then((res)=>res.json())
-    .then((res2)=>{
-        console.log(res2)
-        let {name,sprites,stats,types}=res2
-    })
-}
+//     let pokemon = getPokemon(1)
+//     pokemon.then((res)=>res.json())
+//     .then((res2)=>{
+//         console.log(res2)
+//         let {name,sprites,stats,types}=res2
+//     })
+// }
 
-//funcion n(umero aleatorio
-function numeroAleatorio(max){
-    return Math.floor(Math.random()*max)
-}
+// //funcion n(umero aleatorio
+// function numeroAleatorio(max){
+//     return Math.floor(Math.random()*max)
+// }
 
-//llenar informacion pokemon
+// //llenar informacion pokemon
 
-function llenarInformacion(name,sprites,stats,types){
-    let input= document.getElementById("input_pokemon")
-    input.placeholder = "ejemplo: "+name
-}
+// function llenarInformacion(name,sprites,stats,types){
+//     let input= document.getElementById("input_pokemon")
+//     input.placeholder = "ejemplo: "+name
+
+//     let tabla = ('<table class="table">  <tr><td> ${name} </td></tr>')
+
+
+// }
+
+const pokemonesCapturados = [];
+
+  fetch('https://pokeapi.co/api/v2/pokemon?limit=100')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(pokemon => {
+      fetch(pokemon.url)
+        .then(response => response.json())
+        .then(pokemonData => {
+            pokemonesCapturados.push(pokemonData)
+        })
+        .catch(error => {
+          console.log('Ha ocurrido un error:', error);
+        });
+    });
+    localStorage.setItem("listaPokemones",JSON.stringify(pokemonesCapturados))
+  })
+  .catch(error => {
+    console.log('Ha ocurrido un error:', error);
+  });
+
+
+  const pokemonName = 'pikachu';
+
+fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+  .then(response => response.json())
+  .then(data => {
+    console.log('Nombre:', data.name);
+    console.log('Altura:', data.height);
+    console.log('Peso:', data.weight);
+    console.log('Habilidades:', data.abilities.map(ability => ability.ability.name).join(', '));
+    console.log('Tipo:', data.types.map(type => type.type.name).join(', '));
+    console.log('Estadísticas:');
+    data.stats.forEach(stat => {
+      console.log(`${stat.stat.name}: ${stat.base_stat}`);
+    });
+  })
+  .catch(error => {
+    console.log('Ha ocurrido un error:', error);
+  });
+
+
